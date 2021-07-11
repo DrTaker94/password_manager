7e6370815c849d4da836c06a6afe6edd8550ec9b
Hasan <hassan.h.sayfi@gmail.com>
2021-07-11 21:13:47 +0300

    initiate Password Manager App

---
+++ android/app/src/main/kotlin/sa/cerebra/passwordmanager/password_manager/MainActivity.kt
@@ -0,0 +1,6 @@
+package sa.cerebra.passwordmanager.password_manager
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(PasswordManager());
+}
+
+class PasswordManager extends StatefulWidget {
+
+  @override
+  _PasswordManagerState createState() => _PasswordManagerState();
+}
+
+class _PasswordManagerState extends State<PasswordManager> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
