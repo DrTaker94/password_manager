7727f0a1e467cd52469ece30e0080a0ae73b3188
Hasan <hassan.h.sayfi@gmail.com>
2021-07-13 22:48:20 +0300

    Create Password Information skeleton

---
+++ lib/model/password_info.dart
@@ -0,0 +1,17 @@
+class PasswordInfo {
+  final int id;
+  final String title;
+  final String username;
+  final String password;
+  final String url;
+  final String notes;
+
+  const PasswordInfo({
+    required this.id,
+    required this.title,
+    required this.username,
+    required this.password,
+    required this.url,
+    required this.notes,
+  });
+}
